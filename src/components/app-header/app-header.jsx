40383cd10fad34c3ea3113344ba2635f30d3622b
stephazub <stepazubenko@outlook.com>
2023-08-04 18:01:42 +0300

    Header готов

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -4,31 +4,31 @@ import styles from './app-header.module.css';
 export function AppHeader() {
     return (
         <header className={styles.header}>
-            <nav className={styles.navigation}>
-                <ul className={styles.list}>
+            <nav>
+                <ul className={styles.nav}>
                     <div className={styles.columns}>
-                        <li>
+                        <li className="pl-5 pr-5 pt-4 pb-4">
                             <a href="#" className={styles.link}>
                                 <BurgerIcon type="primary" />
-                                <p>Конструктор</p>
+                                <p className="text text_type_main-default">Конструктор</p>
                             </a>
                         </li>
-                        <li>
+                        <li className="pl-5 pr-5 pt-4 pb-4">
                             <a href="#" className={styles.link}>
                                 <ListIcon type="secondary" />
-                                <p>Лента заказов</p>
+                                <p className="text text_type_main-default text_color_inactive">Лента заказов</p>
                             </a>
                         </li>
                     </div>
-                    <li className={styles.logo}>
-                        <a href="#" className={styles.link}>
+                    <li>
+                        <a href="#">
                             <Logo />
                         </a>
                     </li>
                     <li className={styles.personal_account}>
-                        <a href="#" className={styles.link}>
+                        <a href="#" className={`${styles.link} pl-5 pr-5 pt-4 pb-4`}>
                             <ProfileIcon type="secondary" />
-                            <p>Личный кабинет</p>
+                            <p className="text text_type_main-default text_color_inactive">Личный кабинет</p>
                         </a>
                     </li>
                 </ul>
