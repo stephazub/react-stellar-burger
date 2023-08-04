d9c1023e99eb3ea639873dc73253210ce5bde926
stephazub <stepazubenko@outlook.com>
2023-08-04 15:30:54 +0300

    npm init

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,11 @@
-import styles from "./app.module.css";
-import { data } from "../../utils/data";
+import React from 'react';
+import { AppHeader } from '../app-header/AppHeader';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
-    </div>
+    <>
+      <AppHeader />
+    </>
   );
 }
 
