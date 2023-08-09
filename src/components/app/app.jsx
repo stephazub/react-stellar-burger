a7c5c7cc68a3bb3288c006d250280ac5480e140a
stephazub <stepazubenko@outlook.com>
2023-08-09 17:43:30 +0300

    поправил

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppHeader } from '../AppHeader/AppHeader';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
-import styles from './App.module.css';
+import styles from './app.module.css';
 import {ingredientsData} from '../api/api';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
 import Modal  from '../Modal/Modal';
