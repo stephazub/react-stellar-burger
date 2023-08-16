94aa6c2b378a4131c80d67f1599f83411d566dff
stephazub <stepazubenko@outlook.com>
2023-08-16 18:33:16 +0300

    Готов таб ингридиентов

--- src/components/BurgerIngredientsSets/BurgerIngredientsSets.jsx
+++ src/components/BurgerIngredientsSets/BurgerIngredientsSets.jsx
@@ -1,8 +1,58 @@
 import React from "react";
 import styles from './BurgerIngredientsSets.module.css';
-import { ingredientPropType } from '../../utils/prop-types';
-import PropTypes from 'prop-types';
-import BurgerIngredientsItem from "../BurgerIngredientsItem/BurgerIngredientsItem";
+import BurgerIngredientsSet from '../BurgerIngredientsSet/BurgerIngredientsSet';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRef, useEffect } from 'react';
+import { setActiveTab } from '../../services/action/burgerIngredientsTab';
+
+export default function BurgerIngredientsSets() {
+    const dispatch = useDispatch()
+    const tab = useSelector(state => state.ingredientsTab.tab)
+
+    const bunRef = useRef();
+    const sauceRef = useRef();
+    const mainRef = useRef();
+    const tabRef = useRef();
+
+    useEffect(() => {
+        if (tab === 'bun') {
+            bunRef.current.scrollIntoView({ behavior: 'smooth' })
+        }
+        if (tab === 'sauce') {
+            sauceRef.current.scrollIntoView({ behavior: 'smooth' })
+        }
+        if (tab === 'main') {
+            mainRef.current.scrollIntoView({ behavior: 'smooth' })
+        }
+    }, [tab])
+
+    useEffect(() => {
+        const headings = [
+            bunRef.current,
+            sauceRef.current,
+            mainRef.current
+        ]
+        const observer = new IntersectionObserver((headings) => {
+            headings.forEach((heading) => {
+                if (heading.target === bunRef.current) {
+                    dispatch(setActiveTab('bun'))
+                }
+                if (heading.target === sauceRef.current) {
+                    dispatch(setActiveTab('sauce'))
+                }
+                if (heading.target === mainRef.current) {
+                    dispatch(setActiveTab('main'))
+                }
+            })
+
+        },
+            {
+                root: tabRef.current,
+                rootMargin: '0px 0px -90% 0px'
+            })
+        headings.forEach((heading) => observer.observe(heading))
+
+    }, [dispatch])
 
 export default function BurgerIngredientsSets ({ingredients, handleIngredientClick}) {
     return (
